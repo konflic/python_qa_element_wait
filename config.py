b6e0480b04021b404d88324c7ab037174675d952
konflic <konflic@mail.ru>
2021-05-18 17:17:56 +0300

    Fix path to drivers

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-DRIVERS = "/home/mchirkov/Downloads/drivers"
+import os
+
+
+DRIVERS = os.path.expanduser("~/Downloads/drivers")
 CHROMEDRIVER = f"{DRIVERS}/chromedriver"
 GECKODRIVER = f"{DRIVERS}/geckodriver"
 OPERADRIVER = f"{DRIVERS}/operadriver"
